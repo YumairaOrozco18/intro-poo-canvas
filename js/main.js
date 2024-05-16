199305d62ef6d3103a77a84a80caeac243e88bbb
YumairaOrozco18 <116225113+YumairaOrozco18@users.noreply.github.com>
2024-05-16 01:29:21 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,37 +2,42 @@ const canvasOOP = document.getElementById("canvasOOP");
 const canvasRandom = document.getElementById("canvasRandom");
 const canvasMultiple = document.getElementById("canvasMultiple");
 
-const ctx = canvasOOP.getContext("2d"); //ctx=contexto
-const ctxRandom = canvasRandom.getContext("2d"); 
-const ctxMultiple = canvasMultiple.getContext("2d"); 
 
-/*const window_height = window.innerHeight; //Ajusta tamaño de la pagina
-const window_width = window.innerWidth;*/
+const ctx = canvasOOP.getContext("2d");
+const ctxRandom = canvasRandom.getContext("2d");
+const ctxMultiple = canvasMultiple.getContext("2d");
 
-canvasOOP.height = "200"; /*window_height; //el canvas tendra el ancho de la pantalla*/
-canvasOOP.width = "300"; /*window_width;*/ // se escribe solo el numero sin el px
-canvasRandom.height = "200"; /*window_height; //el canvas tendra el ancho de la pantalla*/
-canvasRandom.width = "300"; /*window_width;*/ // se escribe solo el numero sin el px
-canvasMultiple.height = "200"; /*window_height; //el canvas tendra el ancho de la pantalla*/
-canvasMultiple.width = "300"; /*window_width;*/ // se escribe solo el numero sin el px
+
+/* const window_height = window.innerHeight;
+const window_width = window.innerWidth; */
+
+
+canvasOOP.height = "200";//window_height;
+canvasOOP.width = "300"//window_width;
+canvasRandom.height = "200";//window_height;
+canvasRandom.width = "300"//window_width;
+canvasMultiple.height = "200";//window_height;
+canvasMultiple.width = "300"//window_width;
 
 canvasOOP.style.background = "#ff8";
 canvasRandom.style.background = "F0DCA5";
 canvasMultiple.style.background = "#7DB3C7";
 
-class Circle {
 
-    // constructor que carga los valores determinados que carga el objeto
-    constructor(x, y, radius, color, text, backcolor) { // x & y posicion que ocupara el circulo 
-        this.posX = x; // con el this creamos propiedades
+
+class Circle {
+    // carga los  valores predeterminados del objeto
+    constructor(x, y, radius, color, text, backcolor) {
+        this.posX = x;
         this.posY = y;
         this.radius = radius;
         this.color = color;
         this.text = text;
         this.backcolor = backcolor;
+
     }
 
-    //metodo para renderizar el objeto
+    // metodo para renderizar 
     draw(context) {
         //rellena el objeto
         context.beginPath();
@@ -57,29 +62,28 @@ class Circle {
     }
 }
 
-let randomRadius = Math.floor(Math.random() * 25 + 30); 
-
-let miCirculo = new Circle(canvasOOP.width/2, canvasOOP.height/2, 50, 'red','Tec', 'blue'); //coordenadas iniciales no superan el tamaño del canvas
+let miCirculo = new Circle(canvasOOP.width / 2, canvasOOP.height / 2, 50, 'red', 'Tec', 'Blue');
 miCirculo.draw(ctx);
+
+let randomRadius = Math.floor(Math.random() * 25 + 30); // Radio aleatorio entre 30 y 40
 
 // Asegúrate de que el círculo no se salga de los bordes del canvas
 let randomX = Math.random() * (canvasRandom.width - 2 * randomRadius) + randomRadius;
 let randomY = Math.random() * (canvasRandom.height - 2 * randomRadius) + randomRadius;
 
-let miCirculoRandom = new Circle(randomX, randomY,randomRadius,'red','Tec', 'blue'); //coordenadas iniciales no superan el tamaño del canvas
+let miCirculoRandom = new Circle(randomX, randomY, randomRadius, 'Red', 'Tec', 'Blue');
 miCirculoRandom.draw(ctxRandom);
 
-let arrayCircle =[];
 
+let arrayCircle = [];
 for (let i = 0; i < 10; i++) {
-
-    let randomX = Math.random() * (canvasMultiple.width-2 * randomRdius);
-    let randomY = Math.random() * (canvasMultiple.height-2 * randomRdius);
     let randomRadius = Math.floor(Math.random() * 10 + 30);
-  
-    let miCirculoMultiple = new Circle(randomX, randomY, randomRadius, "blue", i + 1, 'yellow');
-  
+    let randomX = Math.random() * (canvasMultiple.width - 2 * randomRadius) + randomRadius;
+    let randomY = Math.random() * (canvasMultiple.height - 2 * randomRadius) + randomRadius;
+    
+    let miCirculoMultiple = new Circle(randomX, randomY, randomRadius, "blue", i + 1, "yellow");
+
     arrayCircle.push(miCirculoMultiple);
+
     arrayCircle[i].draw(ctxMultiple);
-  
-  }
+}
